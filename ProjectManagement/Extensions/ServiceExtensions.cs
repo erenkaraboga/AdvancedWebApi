d9f564b561af733c45908387b7f3fc2fe5d7cfe0
Eren Karaboğa <darkbull1073@gmail.com>
2022-05-21 20:54:32 +0300

    Cors Configuration Added

---
+++ ProjectManagement/Extensions/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectManagement.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                 builder.AllowAnyOrigin()
+                 .AllowAnyMethod()
+                 .AllowAnyHeader());
+            });
+        }
+    }
+}
--- ProjectManagement/Program.cs
+++ ProjectManagement/Program.cs
@@ -1,3 +1,5 @@
+using ProjectManagement.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.ConfigureCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,5 +23,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("CorsPolicy");
 
 app.Run();
