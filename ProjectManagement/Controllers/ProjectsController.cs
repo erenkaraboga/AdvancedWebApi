a58725b7ff5c2cefd05f95d8540c929c927a7cd9
Eren Karaboğa <darkbull1073@gmail.com>
2022-05-21 21:04:45 +0300

    Entities Added

---
+++ ProjectManagement/Controllers/ProjectsController.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    [Route("api/projects")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private List<Project> _projectList;
+        public ProjectsController()
+        {
+            _projectList = new List<Project>()
+            {
+                new Project() { Id = Guid.NewGuid(), Name ="Project1"},
+                new Project() { Id = Guid.NewGuid(), Name="Project2"},
+            }; 
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_projectList);
+        }
+    }
+}
