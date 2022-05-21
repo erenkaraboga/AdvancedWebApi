46aac0823e14c7fd63a82e391c49e5ae8ea15ecc
Eren Karaboğa <darkbull1073@gmail.com>
2022-05-21 22:45:45 +0300

    Data Annotations Added

---
+++ Entities/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Employee
+    {
+        [Column("EmployeeId")]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="FirstName is Required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "LastName is Required")]
+        public string LastName { get; set; }
+        public int? Age { get; set; }
+        public string? Position { get; set; }
+
+        [ForeignKey(nameof(Project))]
+        public Guid? ProjectId { get; set; }
+        public Project Project { get; set; }
+
+    }
+}
--- Entities/Models/Project.cs
+++ Entities/Models/Project.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,15 @@ namespace Entities.Models
 {
     public class Project
     {
+        [Column("ProjectId")]
         public Guid Id { get; set; }
+       
+        [Required(ErrorMessage ="Project name is required")]
+        [MaxLength(60,ErrorMessage ="Max lenght 60 charecter")]
         public string Name { get; set; }
-
+        public string? Description { get; set; }
+        public string? Field { get; set; }
+        public string? ImageUrl { get; set; }
+        public ICollection<Employee>? Employees { get; set;}
     }
 }
