7a1793b5e6b6991bccc7b3e2326a2ec6a9be4d13
Eren Karaboğa <darkbull1073@gmail.com>
2022-05-22 10:17:26 +0300

    RepositoryBase

--- Contracts/IRepositoryBase.cs
+++ Contracts/IRepositoryBase.cs
@@ -10,7 +10,7 @@ namespace Contracts
     public interface IRepositoryBase<T>where T : class
     {
         IQueryable<T> FindAll(bool trackChanges);
-        IQueryable<T> FindByCondition(Expression<Func<bool>> expression, bool trackChanges);
+        IQueryable<T> FindByCondition(Expression<Func<T,bool>> expression, bool trackChanges);
         void Create(T entity);
         void Update(T entity);
         void Delete(T entity);
